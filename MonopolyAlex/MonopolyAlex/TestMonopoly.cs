397d95532e2a7aedee8f0b421f7bdda09e18a358
ShadowKatt21 <alex.o.botez@gmail.com>
2024-11-04 18:55:47 +0000

    refactor: beutifying

--- MonopolyAlex/MonopolyAlex/TestMonopoly.cs
+++ MonopolyAlex/MonopolyAlex/TestMonopoly.cs
@@ -6,87 +6,87 @@ namespace MonopolyAlex;
 public class TestMonopoly
 {
     private Player _player;
-        private Property _property;
-        private HouseService _houseService;
-        private PlayerRepositoryStub _playerRespositoryStub;
-        private PropertyRepositoryStub _propertyRepositoryMock;
+    private Property _property;
+    private HouseService _houseService;
+    private PlayerRepositoryStub _playerRespositoryStub;
+    private PropertyRepositoryStub _propertyRepositoryMock;
 
-        [TestInitialize]
-        public void Setup()
-        {
-            _player = TestHelper.CreatePlayer();
-            _property = TestHelper.CreateProperty("Vine Street", "Orange");
+    [TestInitialize]
+    public void Setup()
+    {
+        _player = TestHelper.CreatePlayer();
+        _property = TestHelper.CreateProperty("Vine Street", "Orange");
 
-            _playerRespositoryStub = new PlayerRepositoryStub();
-            _propertyRepositoryMock = new PropertyRepositoryStub();
+        _playerRespositoryStub = new PlayerRepositoryStub();
+        _propertyRepositoryMock = new PropertyRepositoryStub();
 
-            _houseService = new HouseService(_playerRespositoryStub, _propertyRepositoryMock);
-        }
-        [TestMethod]
-        public void ShouldnotBeAbleToBuyAHouseIfPropertyHasMaxHouses()
-        {
-           _property.Houses = 4;
-           _houseService.BuyHouse(_property, "Battle Ship");
+        _houseService = new HouseService(_playerRespositoryStub, _propertyRepositoryMock);
+    }
+    [TestMethod]
+    public void NotAbleToBuyAHouseIfPropertyHasMaxHouses()
+    {
+       _property.Houses = 4;
+       _houseService.BuyHouse(_property, "Battle Ship");
 
-            Assert.AreEqual(_property.Houses, 4);
-        }
+        Assert.AreEqual(_property.Houses, 4);
+    }
 
-        [TestMethod]
-        public void ShouldNotBeAbleToBuyAHouseIfAPlayerDoesNotOwnAllThePropertiesOfTheColour()
-        {
-            _player.Properties.RemoveAt(0);
-            _playerRespositoryStub.PlayerToReturn = _player;
-            _houseService.BuyHouse(_property, "Battle Ship");
+    [TestMethod]
+    public void NotBeAbleToBuyAHouseIfAPlayerDoesNotOwnAllThePropertiesOfTheColour()
+    {
+        _player.Properties.RemoveAt(0);
+        _playerRespositoryStub.PlayerToReturn = _player;
+        _houseService.BuyHouse(_property, "Battle Ship");
 
-            Assert.AreEqual(_property.Houses, 0);
-        }
+        Assert.AreEqual(_property.Houses, 0);
+    }
 
-        [TestMethod]
-        public void ShouldBeAbleToBuyAHouseIfAPlayerOwnsAllThePropertiesOfTheColour()
-        {
-            _playerRespositoryStub.PlayerToReturn = _player;
+    [TestMethod]
+    public void ShouldBeAbleToBuyAHouseIfAPlayerOwnsAllThePropertiesOfTheColour()
+    {
+        _playerRespositoryStub.PlayerToReturn = _player;
 
 
-            _houseService.BuyHouse(_property, "Battle Ship");
+        _houseService.BuyHouse(_property, "Battle Ship");
 
-            Assert.AreEqual(_property.Houses, 1);
-        }
+        Assert.AreEqual(_property.Houses, 1);
+    }
 
-        [TestMethod]
-        public void ShouldNotBeAbleToBuyAHouseIfThebankRollIsLessThanTheCostOfTheHouse()
-        {
-            _player.BankRoll = 0;
-            _playerRespositoryStub.PlayerToReturn = _player;
+    [TestMethod]
+    public void ShouldNotBeAbleToBuyAHouseIfThebankRollIsLessThanTheCostOfTheHouse()
+    {
+        _player.BankRoll = 0;
+        _playerRespositoryStub.PlayerToReturn = _player;
 
-            _houseService.BuyHouse(_property, "Battle Ship");
+        _houseService.BuyHouse(_property, "Battle Ship");
 
-            Assert.AreEqual(_property.Houses, 0);
-        }
+        Assert.AreEqual(_property.Houses, 0);
+    }
 
-        [TestMethod]
-        public void ShouldDeductHouseCostFromThePlayersMoney()
-        {
-            _playerRespositoryStub.PlayerToReturn = _player;
+    [TestMethod]
+    public void ShouldDeductHouseCostFromThePlayersMoney()
+    {
+        _playerRespositoryStub.PlayerToReturn = _player;
 
-            _houseService.BuyHouse(_property, "Battle Ship");
-            Assert.AreEqual(1900, _player.BankRoll);
-        }
+        _houseService.BuyHouse(_property, "Battle Ship");
+        Assert.AreEqual(1900, _player.BankRoll);
+    }
 
-        [TestMethod]
-        public void ShouldSaveProperty()
-        {
-            
-            _houseService.BuyHouse(_property, "Battle Ship");
-
-            Assert.IsTrue(_propertyRepositoryMock.HasSavePropertyBeenCalled);
-        }
-
-        [TestMethod]
-        public void ShouldSavePlayer()
-        {
+    [TestMethod]
+    public void ShouldSaveProperty()
+    {
         
+        _houseService.BuyHouse(_property, "Battle Ship");
 
-            _houseService.BuyHouse(_property, "Battle Ship");
-            Assert.IsTrue(_playerRespositoryStub.WasPlayerSaved);
-        }
+        Assert.IsTrue(_propertyRepositoryMock.HasSavePropertyBeenCalled);
+    }
+
+    [TestMethod]
+    public void ShouldSavePlayer()
+    {
+    
+
+        _houseService.BuyHouse(_property, "Battle Ship");
+        Assert.IsTrue(_playerRespositoryStub.WasPlayerSaved);
+    }
 }
